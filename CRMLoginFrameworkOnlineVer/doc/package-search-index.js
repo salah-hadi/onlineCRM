d488c8488452ef6647c425b4e7a2c49b62f77c6c
salah-hadi <49116004+salah-hadi@users.noreply.github.com>
2019-08-18 16:33:47 +0200

    renaming pHeaderButtons to PFormButtons add HomePageButtons

---
+++ CRMLoginFrameworkOnlineVer/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"allclasses"},{"l":"All Packages","url":"allpackages-index.html"}]
---
+++ CRMLoginFrameworkOnlineVer/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"allclasses","l":"commonParaFun"},{"p":"allclasses","l":"login"}]
